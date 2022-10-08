706def79a107c489ef8461a114874d7dfbdd235c
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-08 14:44:51 +0530

    Removed unnecessary dependencies

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const StartButton = ({ level, nextSequence,isStarted ,setIsStarted}) => {
+const StartButton = ({ nextSequence, isStarted, setIsStarted }) => {
 
     return (
         <div className="absolute bottom-20 sm:bottom-14 md:bottom-8 lg:bottom-4 left-1/2 -translate-x-[50%]">
-          {!isStarted &&  <button
+            {!isStarted && <button
                 className={` px-5 py-3 mt-5 text-[#fef2bf] bg-black rounded-full items-center`}
                 onClick={() => {
                     setIsStarted(true)
