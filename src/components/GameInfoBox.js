461ba07816cc7861cf22265309b017523d8b27dd
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2022-10-08 15:52:52 +0530

    Some minor change in GameInfo.js file

--- src/components/GameInfoBox.js
+++ src/components/GameInfoBox.js
@@ -6,7 +6,7 @@ const GameInfo = ({ reference, showInfo, setShowInfo }) => {
         "After a few seconds the game will start.",
         "When a color Box flicker remeber the box and click on it.",
         "If you click on the correct box then a new/same box flicker again.",
-        "Andnow you have to click all the previous boxes and then this box.",
+        "And now you have to click all the previous boxes and then this box.",
         "in the sequence if you click on any wrong box then that will be wrong answer",
         "If your answer is wrong then the stat button appears again, ang heading changes to 'Game Over start Again'"
 
